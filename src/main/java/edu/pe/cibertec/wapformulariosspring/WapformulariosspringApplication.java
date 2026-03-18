20eaf3a36c61001c2fbb066e8003d2d56a12e2a9
Larry Rimache López <larry_rimache_lopez_20@hotmail.com>
2026-03-18 12:14:55 -0500

    Diseño login terminado

---
+++ src/main/java/edu/pe/cibertec/wapformulariosspring/WapformulariosspringApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.wapformulariosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WapformulariosspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WapformulariosspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/cibertec/wapformulariosspring/WapformulariosspringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.wapformulariosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WapformulariosspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
