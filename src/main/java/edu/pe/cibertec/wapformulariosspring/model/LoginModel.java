2e153180b659810f3f08a84f42a988ee6951cf71
Larry Rimache López <larry_rimache_lopez_20@hotmail.com>
2026-03-18 13:52:49 -0500

    Vista y Controller terminados

---
+++ src/main/java/edu/pe/cibertec/wapformulariosspring/model/LoginModel.java
@@ -0,0 +1,11 @@
+package edu.pe.cibertec.wapformulariosspring.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginModel {
+    private String usuario;
+    private String password;
+}
