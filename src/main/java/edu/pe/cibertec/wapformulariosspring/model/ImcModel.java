fe977f7dabad4279773e606b596afd493ec32fe3
Larry Rimache López <larry_rimache_lopez_20@hotmail.com>
2026-03-20 13:38:55 -0500

    IMC terminado

---
+++ src/main/java/edu/pe/cibertec/wapformulariosspring/model/ImcModel.java
@@ -0,0 +1,11 @@
+package edu.pe.cibertec.wapformulariosspring.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImcModel {
+    private double talla;
+    private double peso;
+}
